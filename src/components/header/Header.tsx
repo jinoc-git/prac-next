6ad7e0be0a6acfd6db827a78f72b32a7a9e24485
jinoc <choiae369@gmail.com>
2024-01-04 22:16:32 +0900

    Feat: supabase 타입 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,26 @@
+'use client';
+
 import React from 'react';
 
+import { usePathname } from 'next/navigation';
+
+import Logo from './logo/Logo';
+
 export default function Header() {
+  const pathname = usePathname();
+  const bgWhite =
+    pathname !== '/' &&
+    pathname !== '/signin' &&
+    pathname !== '/signup' &&
+    pathname !== '/main';
+
   return (
-    <header className="flex justify-between items-center fixed w-screen pr-3 z-30 sm:h-[90px] md:h-[70px]">
-      Header
+    <header
+      className={`flex justify-between items-center fixed w-screen pr-3 z-30 sm:h-[90px] md:h-[70px]
+      ${bgWhite ? 'bg-bg_white' : 'bg-transparent'}
+      `}
+    >
+      <Logo />
     </header>
   );
 }
