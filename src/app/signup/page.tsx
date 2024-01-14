62742b984bf8e090ebcdc1348e02db070121dfdf
jinoc <choiae369@gmail.com>
2024-01-15 02:15:00 +0900

    Fix: SignupForm 다이나믹 삭제

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-import dynamic from 'next/dynamic';
-
-const DynamicSignupForm = dynamic(
-  () => import('@/components/common/form/SignupForm'),
-  {
-    ssr: false,
-  },
-);
+import SignupForm from '@/components/common/form/SignupForm';
 
 export default function Signup() {
-  return <DynamicSignupForm />;
+  return <SignupForm />;
 }
