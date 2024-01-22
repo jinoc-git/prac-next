92b2389aac64da90e8ca342026ab3cdffd38d1fd
jinoc <choiae369@gmail.com>
2024-01-23 01:18:47 +0900

    Feat: pathname이 main일 때 로고 컬러 변경

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -8,14 +8,14 @@ import SideBarIcon from './SideBarIcon';
 
 export default function SideBar() {
   const { isVisibleSideBar, isSideBarOpen } = sideBarStore();
-  console.log('sd', isVisibleSideBar);
+
   return isVisibleSideBar ? (
     <>
       <SideBarIcon />
       <aside
         className={` touch-none fixed h-[100vh] border-r border-slate-300 rounded-r-[12px] z-[31] overflow-hidden bg-white side-bar-transition  ${
           isSideBarOpen
-            ? 'sm:w-[357px] sm:px-[24px] md:w-[270px] md:px-[24px]  '
+            ? 'sm:w-[357px] sm:px-[24px] md:w-[270px] md:px-[24px] '
             : 'sm:w-[0px] sm:px-[0px] md:w-[88px] md:px-[24px]'
         }`}
       >
