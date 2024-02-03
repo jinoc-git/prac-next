89392492695e299846b8658b09b07ae0a12fec8b
jinoc <choiae369@gmail.com>
2024-02-04 02:43:14 +0900

    Feat: 사이드바 현재 여행 표시하는 부분 작업중

--- src/components/sideBar/SideBarStatus.tsx
+++ src/components/sideBar/SideBarStatus.tsx
@@ -2,6 +2,12 @@
 
 import React from 'react';
 
+import { useQuery } from '@tanstack/react-query';
+
+import { getPlanListAndMateList } from '@/api/plan';
+import { authStore } from '@/store/authStore';
+import { sideBar } from '@/utils/arrayCallbackFunctionList';
+
 import SideBarStatusChip from './SideBarStatusChip';
 
 interface SideBarStatusProps {
@@ -10,6 +16,34 @@ interface SideBarStatusProps {
 
 export default function SideBarStatus(props: SideBarStatusProps) {
   const { isOpen } = props;
+  const user = authStore((state) => state.user);
+
+  const { data: matesData, isError: matesError } = useQuery({
+    queryKey: ['plan_mates', user?.id],
+    queryFn: async () => {
+      return await getPlanListAndMateList(user === null ? '' : user.id);
+    },
+    enabled: user !== null,
+    staleTime: 20 * 1000,
+  });
+
+  const sortedData = matesData?.planDataList?.sort(sideBar.sorting);
+
+  const startPlans = sortedData?.filter(sideBar.filtering('planning'));
+  const endPlans = sortedData?.filter(sideBar.filtering('end'));
+  const activePlan = sortedData?.find(sideBar.filtering('traveling'));
+
+  const nextPlan = startPlans != null ? startPlans[0] : undefined;
+  const hasNextPlan = Boolean(nextPlan);
+
+  const hasActivePlan = activePlan !== undefined;
+
+  const status = hasActivePlan
+    ? '여행 중'
+    : hasNextPlan
+      ? '여행 예정'
+      : '여행 없음';
+
   return (
     <div
       className="flex flex-col items-center border-slate-200
@@ -22,7 +56,7 @@ export default function SideBarStatus(props: SideBarStatusProps) {
         }`}
       >
         <span className="font-semibold text-xs text-gray_dark_1">STATUS</span>
-        <SideBarStatusChip isOpen={isOpen} status={'여행 없음'} />
+        <SideBarStatusChip isOpen={isOpen} status={status} />
       </div>
     </div>
   );
