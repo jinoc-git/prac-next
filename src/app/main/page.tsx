d69a8266c5f0d36be58cb9b3c0281aafad26ee3a
jinoc <choiae369@gmail.com>
2024-02-07 18:57:17 +0900

    Feat: 메인 프로필 컴포넌트 추가:

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <div
+        className="absolute top-0 left-0 w-full bg-blue_dark z-[-1]
+        sm:h-[313px]
+        md:h-[363px]"
+      ></div>
+    </>
+  );
 }
