a8926715d7bcb78227dc31e1afa8fff1229201c3
jinoc <choiae369@gmail.com>
2024-02-09 02:34:08 +0900

    Feat: 메인 탭메뉴 스토어 생성

--- src/components/main/planList/PlanList.tsx
+++ src/components/main/planList/PlanList.tsx
@@ -5,16 +5,19 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 
 import AddPlanBtn from './AddPlanBtn';
+import PlanTabMenu from './PlanTabMenu';
 
 export default function PlanList() {
   const router = useRouter();
+
   return (
     <section
-      className="flex mx-auto my-0
+      className="flex flex-col mx-auto my-0
         sm:w-[320px] 
         md:w-[800px] "
     >
       <AddPlanBtn />
+      <PlanTabMenu />
     </section>
   );
 }
---
+++ src/components/main/planList/PlanTabMenu.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+interface PlanTabMenuProps {}
+
+export default function PlanTabMenu(props: PlanTabMenuProps) {
+  const {} = props;
+
+  return (
+    <div className="flex flex-col gap-[16px]">
+      <div></div>
+    </div>
+  );
+}
