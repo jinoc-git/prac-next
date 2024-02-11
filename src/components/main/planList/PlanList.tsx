1566f1b47c6272d0ea1211037ae5d22c8929d7d2
jinoc <choiae369@gmail.com>
2024-02-11 22:22:53 +0900

    Feat: PlanList.tsx에서 북마크 데이터와 자신이 포함된 여행 정보 불러오는 api 함수 호출

--- src/components/main/planList/PlanList.tsx
+++ src/components/main/planList/PlanList.tsx
@@ -2,13 +2,41 @@
 
 import React from 'react';
 
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
+import { getBookMarkDataByUserId } from '@/api/bookMark';
+import { getPlanListAndMateList } from '@/api/plan';
+import { authStore } from '@/store/authStore';
+
 import AddPlanBtn from './AddPlanBtn';
+import PlanCardList from './PlanCardList';
 import PlanTabMenu from './planTabMenu/PlanTabMenu';
 
 export default function PlanList() {
   const router = useRouter();
+  const user = authStore((state) => state.user);
+
+  const {
+    data: matesData,
+    isLoading: matesLoading,
+    isError: matesError,
+  } = useQuery({
+    queryKey: ['plan_mates', user?.id],
+    queryFn: async () => await getPlanListAndMateList(user?.id),
+    enabled: user !== null,
+  });
+
+  const {
+    data: bookMarkData,
+    isLoading: bookMarkLoading,
+    isError: bookMarkError,
+  } = useQuery({
+    queryKey: ['book_mark', user?.id],
+    queryFn: async () => await getBookMarkDataByUserId(user?.id),
+    enabled: user !== null,
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <section
@@ -19,6 +47,7 @@ export default function PlanList() {
       <AddPlanBtn />
       <div className="flex flex-col gap-[16px]">
         <PlanTabMenu />
+        <PlanCardList />
       </div>
     </section>
   );
