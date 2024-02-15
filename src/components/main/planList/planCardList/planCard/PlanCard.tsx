d67c0172a913af023e6386c4b47057915bb385f9
jinoc <choiae369@gmail.com>
2024-02-15 23:00:39 +0900

    Feat: 여행 카드 나가기 버튼 추가

--- src/components/main/planList/planCardList/planCard/PlanCard.tsx
+++ src/components/main/planList/planCardList/planCard/PlanCard.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import { calculateDday } from '@/utils/aboutDay';
@@ -30,6 +31,8 @@ export default function PlanCard(props: PlanCardProps) {
     if (status === 'recording') router.push(`/addPhoto/${id}`);
     if (status === 'end') router.push(`/ending/${id}`);
   };
+
+  const onClickQuitBtn = (id: string) => {};
 
   return (
     <div
@@ -69,6 +72,28 @@ export default function PlanCard(props: PlanCardProps) {
         </div>
         <PlanCardDate dates={plan.dates} />
         <PlanCardUserList avatarList={avatarList} nicknameList={nicknameList} />
+      </div>
+      <div
+        className="flex justify-center h-[16px]
+          sm:w-[15px] sm:ml-[10px] sm:mr-[10px] sm:mt-[20px] 
+          md:w-[80px] md:mr-[25px] md:mt-[22px]"
+      >
+        <button
+          className="sm:mt-[2px] md:mt-[4px]"
+          aria-label="card-quit-plan-btn"
+          onClick={() => {
+            onClickQuitBtn(plan.id);
+          }}
+        >
+          <Image
+            alt="여행 나가기"
+            src={'/images/quit.svg'}
+            width={24}
+            height={24}
+            // 호버 시 색상 변경 수정 필요할듯?
+            className=" hover:fill-orange_dark hover:stroke-orange_light_2 sm:w-[16px] sm:h-[16px] md:w-[24px] md:h-[24px]"
+          />
+        </button>
       </div>
     </div>
   );
