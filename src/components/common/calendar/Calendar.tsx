19b5a3aa5838145cc8f1ccae938f784141b211d1
jinoc <choiae369@gmail.com>
2024-02-21 03:29:03 +0900

    Feat: react-datepicker 추가

---
+++ src/components/common/calendar/Calendar.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+
+interface CalendarProps {
+  startDate: Date | null;
+  endDate: Date | null;
+  StartDateChangeHandler: (date: Date | null) => void;
+  EndDateChangeHandler: (date: Date | null) => void;
+}
+
+export default function Calendar(props: CalendarProps) {
+  const { startDate, endDate, StartDateChangeHandler, EndDateChangeHandler } =
+    props;
+
+  return (
+    <div
+      className="relative z-10 flex items-center  
+    sm:w-[286px] sm:block sm:pt-[15px] sm:mx-auto sm:justify-between
+    md:w-[680px] md:py-[10px] md:flex md:mx-[6px]"
+    >
+      <div className="sm:flex sm:justify-between sm:w-[286px] md:w-[280px] ">
+        <div
+          className="flex items-center font-semibold text-gray_dark_1 
+        sm:gap-[7px] sm:mb-[16px]
+        md:gap-2 md:mr-[30px]"
+        >
+          <IconCalendarDefault w="w-[20px]" h="h-[20px]" fill="#4E4F54" />
+          <label className="sm:text-sm sm:w-[63px] md:w-[80px] md:text-normal">
+            여행 시작일
+          </label>
+        </div>
+        <DatePicker
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled,
+          }) => {
+            return (
+              <div className="flex justify-center items-center bg-white m-[10px]">
+                <div className="flex justify-between items-center w-[140px] ">
+                  <button
+                    className="w-[24px] h-[24px] font-Bold text-gray_dark_1"
+                    onClick={decreaseMonth}
+                    disabled={prevMonthButtonDisabled}
+                  >
+                    {'<'}
+                  </button>
+                  <div className="text-[15px] font-SemiBold">
+                    {dayjs(date).format('YYYY년 M월')}
+                  </div>
+                  <button
+                    className="w-[24px] h-[24px] font-Bold text-gray_dark_1"
+                    onClick={increaseMonth}
+                    disabled={nextMonthButtonDisabled}
+                  >
+                    {'>'}
+                  </button>
+                </div>
+              </div>
+            );
+          }}
+          dateFormat="yyyy년 MM월 dd일"
+          shouldCloseOnSelect
+          showIcon
+          selected={startDate}
+          onChange={(date) => {
+            StartDateChangeHandler(date);
+          }}
+          minDate={today}
+          maxDate={endDate}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          locale="ko"
+          dayClassName={(d) =>
+            d.getDate() === startDate?.getDate() ?? false
+              ? `react-datepicker__startDay`
+              : ``
+          }
+          className="text-center react-datepicker read-only:cursor-default 
+            sm:w-[132px] sm:h-[28px]
+            md:w-[120px] md:mr-[46px]"
+          readOnly={modifyState === 'readOnly'}
+          placeholderText="YYYY / MM / DD"
+          required
+          withPortal={screenSize === 'sm'}
+          portalId="datepiker-portal"
+        />
+      </div>
+      <div className="sm:flex sm:justify-between sm:w-[286px] md:w-[280px]">
+        <div
+          className="flex items-center font-semibold text-gray_dark_1
+        sm:gap-[7px] sm:mb-[16px]
+        md:gap-2 md:mr-[30px]"
+        >
+          <IconCalendarDefault w="w-[20px]" h="h-[20px]" fill="#4E4F54" />
+          <label className="sm:text-sm sm:w-[63px] md:w-[80px] md:text-normal">
+            여행 종료일
+          </label>
+        </div>
+        <DatePicker
+          renderCustomHeader={({
+            date,
+            decreaseMonth,
+            increaseMonth,
+            prevMonthButtonDisabled,
+            nextMonthButtonDisabled,
+          }) => {
+            return (
+              <div className="flex justify-center items-center bg-white m-[10px]">
+                <div className="flex justify-between items-center w-[140px] ">
+                  <button
+                    className="w-[24px] h-[24px] font-Bold text-gray_dark_1"
+                    onClick={decreaseMonth}
+                    disabled={prevMonthButtonDisabled}
+                  >
+                    {'<'}
+                  </button>
+                  <div className="text-[15px] font-SemiBold">
+                    {dayjs(date).format('YYYY년 M월')}
+                  </div>
+                  <button
+                    className="w-[24px] h-[24px] font-Bold text-gray_dark_1"
+                    onClick={increaseMonth}
+                    disabled={nextMonthButtonDisabled}
+                  >
+                    {'>'}
+                  </button>
+                </div>
+              </div>
+            );
+          }}
+          dateFormat="yyyy년 MM월 dd일"
+          shouldCloseOnSelect
+          showIcon
+          selected={endDate}
+          onChange={EndDateChangeHandler}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          locale="ko"
+          dayClassName={(d) =>
+            d.getDate() === startDate?.getDate() ?? false
+              ? `react-datepicker__startDay`
+              : ``
+          }
+          className="text-center react-datepicker read-only:cursor-default
+            sm:w-[132px] sm:h-[28px]
+            md:w-[120px] md:mr-[46px]"
+          readOnly={modifyState === 'readOnly'}
+          placeholderText="YYYY / MM / DD"
+          required
+          withPortal={screenSize === 'sm'}
+          portalId="datepiker-portal"
+        />
+      </div>
+    </div>
+  );
+}
