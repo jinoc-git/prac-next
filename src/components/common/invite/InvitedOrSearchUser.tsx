f37dde68cf63eac39207f79e35b536079531a714
jinoc <choiae369@gmail.com>
2024-03-22 03:30:37 +0900

    Feat: searchPeopleModal의 유저 정보 표시하는 컴포넌트 내용 추가
    - next.config에 외부 이미지 불러오기 위해 domains 값 추가

--- src/components/common/invite/InvitedOrSearchUser.tsx
+++ src/components/common/invite/InvitedOrSearchUser.tsx
@@ -2,9 +2,12 @@
 
 import React from 'react';
 
+import Image from 'next/image';
+
 import type { UserType } from '@/types/supabase';
 
 interface InvitedOrSearchUserProps {
+  user: UserType | null;
   person: UserType;
   idx: number;
   handleInvite?: (user: UserType) => Promise<void>;
@@ -12,11 +15,48 @@ interface InvitedOrSearchUserProps {
 }
 
 export default function InvitedOrSearchUser(props: InvitedOrSearchUserProps) {
-  const { person, idx, handleInvite, deleteUser } = props;
+  const { user, person, idx, handleInvite, deleteUser } = props;
 
   return (
-    <div>
-      <div></div>
+    <div className="flex items-center justify-between mr-5 ">
+      <div className="flex items-center justify-start gap-3 my-2 ml-5">
+        {person.avatar_url ? (
+          <Image
+            className="object-cover rounded-full md:w-[45px] md:h-[45px] sm:w-[55px] sm:h-[55px]"
+            src={person.avatar_url}
+            width={45}
+            height={45}
+            alt={`Avatar for ${person.nickname}`}
+          />
+        ) : (
+          <Image
+            className="object-cover rounded-full md:w-[45px] md:h-[45px] sm:w-[55px] sm:h-[55px]"
+            src={'/images/userDefault.svg'}
+            width={45}
+            height={45}
+            alt="Avatar img"
+          />
+        )}
+        <div>
+          <p className="text-normal text-blue">{person.nickname}</p>
+          <p className="sm:text-xs md:text-sm text-gray">{person.email}</p>
+        </div>
+      </div>
+      {user?.id !== person.id && (
+        <button
+          name="userlist-multifunctional-btn"
+          className="md:w-[45px] md:h-[30px] sm:w-[35px] sm:h-[20px] border rounded-lg cursor-pointer text-gray_dark_2 border-gray_dark_1 hover:bg-blue_dark hover:text-white text-xs hover:border-blue font-semibold"
+          onClick={
+            handleInvite
+              ? async () => {
+                  await handleInvite(person);
+                }
+              : () => deleteUser?.(idx)
+          }
+        >
+          {handleInvite != null ? '초대' : '삭제'}
+        </button>
+      )}
     </div>
   );
 }
