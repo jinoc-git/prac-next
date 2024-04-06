c8fa2438a71753a46acf3896ee55871e2a052179
jinoc <choiae369@gmail.com>
2024-04-06 23:11:32 +0900

    Feat: Confirm 컴포넌트 추가
    - RootLayout에 Confirm 컴포넌트 추가

--- src/components/common/layout/ConfirmModalLayout.tsx
+++ src/components/common/layout/ConfirmModalLayout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ConfirmModalLayout = ({ children }: Props) => {
   return createPortal(
-    <div className="fixed inset-0 z-60 flex-box w-screen h-screen bg-black/30 animate-fadeIn">
+    <div className="fixed inset-0 z-50 flex-box w-screen h-screen bg-black/30 animate-fadeIn">
       <div
         className="flex flex-col justify-center bg-bg_white rounded-lg gap-[16px] animate-fadeIn
           md:w-[500px] md:px-[52px] md:py-[48px]
