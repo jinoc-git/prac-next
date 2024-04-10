065abb7b57fd86c53a42f1d110588fa363360dd6
jinoc <choiae369@gmail.com>
2024-04-10 21:49:14 +0900

    Chore: 파일 명 변경에 따른 import 변경

--- src/app/addplan/page.tsx
+++ src/app/addplan/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import AddPlanContents from '@/components/plan/planContents/PlanContents';
+import PlanContents from '@/components/plan/planContents/PlanContents';
 
 export default function AddPlan() {
   return (
     <section>
-      <AddPlanContents />
+      <PlanContents />
     </section>
   );
 }
