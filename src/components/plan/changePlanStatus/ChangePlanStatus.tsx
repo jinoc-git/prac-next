0edf3c3eefa6c004abef63956642f2a8fa64dd4e
jinoc <choiae369@gmail.com>
2024-04-28 02:01:41 +0900

    Fix: handleChangePlanStatus 함수 router의 push path 오타 수정

--- src/components/plan/changePlanStatus/ChangePlanStatus.tsx
+++ src/components/plan/changePlanStatus/ChangePlanStatus.tsx
@@ -42,7 +42,7 @@ const ChangePlanStatus = ({ status, planId }: Props) => {
         '여행을 완료하시면 더 이상 여행 내용을 수정하실 수 없습니다. 완료하시겠습니까?';
       const confFunc = async () => {
         await updatePlanStatus(planId, 'recording');
-        router.push(`/addPhoto/${planId}`);
+        router.push(`/addphoto/${planId}`);
       };
 
       confirm.default(confTitle, confDesc, confFunc);
