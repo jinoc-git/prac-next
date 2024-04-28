b73a5734a30644ec1e74200e54ff218044dac6d1
jinoc <choiae369@gmail.com>
2024-04-28 22:31:59 +0900

    Feat: 사진 추가 페이지 폴더 구조 추가

--- src/app/addphoto/[id]/page.tsx
+++ src/app/addphoto/[id]/page.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-export default function AddPhoto() {
+import { redirect } from 'next/navigation';
+
+import {
+  getAllPinsByPlanFromServer,
+  getPlanByIdFromServer,
+} from '@/api/serverAction';
+import PlanInfo from '@/components/addPhoto/planInfo/PlanInfo';
+
+interface Props {
+  params: { planId: string };
+}
+
+export default async function AddPhoto({ params }: Props) {
+  const { planId } = params;
+
+  const plan = await getPlanByIdFromServer(planId);
+
+  if (plan === null) redirect('/main'); // 잘못된 경로 예정
+
+  const pins = await getAllPinsByPlanFromServer(plan);
+
   return (
-    <section>
-      <div>AddPhoto</div>
-    </section>
+    <>
+      <PlanInfo />
+      <section></section>
+      <section></section>
+    </>
   );
 }
---
+++ src/components/addPhoto/planInfo/PlanInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const PlanInfo = () => {
+  return (
+    <section>
+      <div>PlanInfo</div>
+    </section>
+  );
+};
+
+export default PlanInfo;
