af1eb36e8b55d36ac2a054a492357eb1eca403a2
jinoc <choiae369@gmail.com>
2024-07-07 01:47:36 +0900

    Chore: FCM 확인을 위한 console 추가

--- src/api/sendFCM.ts
+++ src/api/sendFCM.ts
@@ -12,18 +12,20 @@ export interface NotificationData {
 }
 
 export const sendFCMNotification = async (data: NotificationData) => {
-  const serviceAccount: ServiceAccount = {
-    projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-    privateKey: process.env.NEXT_PUBLIC_FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-  };
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  }
   try {
+    const serviceAccount: ServiceAccount = {
+      projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+      privateKey: process.env.NEXT_PUBLIC_FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    };
+
+    if (!admin.apps.length) {
+      console.log('firebase initialize');
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    }
+    console.log('start push');
     const res = await admin.messaging().send(data);
     console.log('result', res, data);
     return res;
