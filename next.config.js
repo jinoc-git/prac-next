172d976c0ba95a10ce8aa8004e7ccd67c4ff1e2b
jinoc <choiae369@gmail.com>
2024-07-08 02:06:42 +0900

    Fix: next.config의 customWorkerDest 옵션 주석

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   customWorkerSrc: '/firebase-messaging-sw.js',
-  customWorkerDest: 'firebase-messaging-sw.js',
+  // customWorkerDest: 'firebase-messaging-sw.js',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
