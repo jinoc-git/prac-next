4f6bd47b885aed4d4cfbcdcc5168bc9691b98fa0
jinoc <choiae369@gmail.com>
2025-01-20 18:35:26 +0900

    Chore: README UPDATE

--- src/utils/changeImgFormatAndCompression.ts
+++ src/utils/changeImgFormatAndCompression.ts
@@ -1,5 +1,29 @@
 import imageCompression from 'browser-image-compression';
 
+// webp를 사용할 지 고민
+// const convertImageToWebP = (file: Blob) => {
+//   return new Promise((resolve, reject) => {
+//     const img = new Image();
+//     img.src = URL.createObjectURL(file);
+//     img.onload = () => {
+//       const canvas = document.createElement('canvas');
+//       canvas.width = img.width;
+//       canvas.height = img.height;
+//       const ctx = canvas.getContext('2d');
+//       ctx?.drawImage(img, 0, 0);
+
+//       canvas.toBlob((blob) => {
+//         if (blob) {
+//           resolve(blob);
+//         } else {
+//           reject(new Error('Image conversion failed'));
+//         }
+//       }, 'image/webp');
+//     };
+//     img.onerror = reject;
+//   });
+// };
+
 export const changeImgFormatAndCompression = async (file: File) => {
   const { default: convertHeicToAnyFormat } = await import('heic2any');
 
